a140ae6e5ae39078885b6e0d0c5a079e390eb63c
Amadatiq <66191804+Amadatiq@users.noreply.github.com>
2021-05-01 10:00:54 -0700

    Add files via upload

---
+++ ProductManagement/index.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="ProductManagement.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:HiddenField ID="hfProductID" runat="server" />
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label Text="Product" runat="server" />
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtProduct" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label Text="Price" runat="server" />
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtPrice" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label Text="Count" runat="server" />
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtCount" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label Text="Description" runat="server" />
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtDescription" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style1"></td>
+                    <td colspan="3" class="auto-style1">
+                        <asp:Button Text="Save" ID="btSave" runat="server" OnClick="btSave_Click"/>
+                        <asp:Button Text="Delete" ID="btDelete" runat="server" OnClick="btDelete_Click" />
+                        <asp:Button Text="Clear" ID="btClear" runat="server" OnClick="btClear_Click" />
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Label Text="" ID="lblSuccessMessage" runat="server" ForeColor="Green" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Label Text="" ID="lblErrorMessage" runat="server" ForeColor="Red" />
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:GridView ID="gvProduct" runat="server" AutoGenerateColumns="false" Width="248px"> 
+                <Columns>
+                    <asp:BoundField DataField="Product" HeaderText="Product" />
+                    <asp:BoundField DataField="Price" HeaderText="Price" />
+                    <asp:BoundField DataField="Count" HeaderText="Count" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton Text="Select" ID="lnkSelect" CommandArgument='<%# Eval("ProductID") %>' runat="server" OnClick="lnkSelect_OnClick"/>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProductManagement/index.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace ProductManagement
+{
+    public partial class index : System.Web.UI.Page
+    {
+        string connectionstring = @"Server=localhost;Database=prodmang;UID=root;Pwd=1234";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Clear();
+                GridFill();
+            }
+        }
+
+        protected void btSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection sqlconn = new MySqlConnection(connectionstring))
+                {
+                    sqlconn.Open();
+                    MySqlCommand sqlCmd = new MySqlCommand("ProductAddOrEdit", sqlconn);
+                    sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("_productid", Convert.ToInt32(hfProductID.Value == "" ? "0" : hfProductID.Value));
+                    sqlCmd.Parameters.AddWithValue("_product", txtProduct.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("_price", Convert.ToDecimal(txtPrice.Text.Trim()));
+                    sqlCmd.Parameters.AddWithValue("_count", Convert.ToInt32(txtCount.Text.Trim()));
+                    sqlCmd.Parameters.AddWithValue("_description", txtDescription.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+                    Clear();
+                    GridFill();
+                    lblSuccessMessage.Text = "Submitted Sucessfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+        void Clear()
+        {
+            hfProductID.Value = "";
+            txtCount.Text = txtDescription.Text = txtProduct.Text = txtPrice.Text = "";
+            btSave.Text = "Save";
+            btDelete.Enabled = false;
+            lblErrorMessage.Text = lblSuccessMessage.Text = "";
+        }
+
+
+        void GridFill()
+        {
+            using (MySqlConnection sqlconn = new MySqlConnection(connectionstring))
+            {
+                sqlconn.Open();
+                MySqlDataAdapter sqlda = new MySqlDataAdapter("ProductView", sqlconn);
+                sqlda.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                System.Data.DataTable dtbl = new System.Data.DataTable();
+                sqlda.Fill(dtbl);
+                gvProduct.DataSource = dtbl;
+                gvProduct.DataBind();
+            }
+        }
+
+        protected void lnkSelect_OnClick(object sender,EventArgs e)
+        {
+            int productID = Convert.ToInt32((sender as LinkButton).CommandArgument);
+            using (MySqlConnection sqlconn = new MySqlConnection(connectionstring))
+            {
+                sqlconn.Open();
+                MySqlDataAdapter sqlda = new MySqlDataAdapter("ProductViewByID", sqlconn);
+                sqlda.SelectCommand.Parameters.AddWithValue("_productid", productID);
+                sqlda.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                System.Data.DataTable dtbl = new System.Data.DataTable();
+                sqlda.Fill(dtbl);
+                txtProduct.Text = dtbl.Rows[0][1].ToString();
+                txtPrice.Text = dtbl.Rows[0][2].ToString();
+                txtCount.Text = dtbl.Rows[0][3].ToString();
+                txtDescription.Text = dtbl.Rows[0][4].ToString();
+                hfProductID.Value = dtbl.Rows[0][0].ToString();
+                btSave.Text = "Update";
+                btDelete.Enabled = true;
+            }
+        }
+
+        protected void btClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        protected void btDelete_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlconn = new MySqlConnection(connectionstring))
+            {
+                sqlconn.Open();
+                MySqlCommand sqlCmd = new MySqlCommand("ProductDeleteByID", sqlconn);
+                sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("_productid", Convert.ToInt32(hfProductID.Value == "" ? "0" : hfProductID.Value));
+                sqlCmd.ExecuteNonQuery();
+                Clear();
+                GridFill();
+                lblSuccessMessage.Text = "Deleted Sucessfully";
+            }
+        }
+    }
+}
